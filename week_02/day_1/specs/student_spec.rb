3c84bfd01b342d2def1e020a5ef59d8a598775ce
mark <markpackham1@gmail.com>
2020-04-06 13:34:55 +0100

    homework/labwork classes

---
+++ week_02/day_1/specs/student_spec.rb
@@ -0,0 +1,8 @@
+require("minitest/autorun")
+require("minitest/reporters")
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+require_relative("../student")
+
+class TestStudent < MiniTest::Test
+end
